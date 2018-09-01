7c0f85fc89bb73b1a8e8450cc8d102fc4f110b43
shwetaopengts <shweta.opengts@gmail.com>
2018-09-01 21:28:29 +0530

    Mongo
    Mongo files

---
+++ mongoose_projects/buildprocess/demo-js-master/build/gen/build_info.c
@@ -0,0 +1,4 @@
+/* Auto-generated, do not edit. */
+const char *build_id = "20180831-015841";
+const char *build_timestamp = "2018-08-31T01:58:41Z";
+const char *build_version = "1.0";
---
+++ mongoose_projects/buildprocess/demo-js-master/build/gen/mg_build_info.c
@@ -0,0 +1,4 @@
+/* Auto-generated, do not edit. */
+const char *mg_build_id = "20180831-015837";
+const char *mg_build_timestamp = "2018-08-31T01:58:37Z";
+const char *mg_build_version = "201808310158";
